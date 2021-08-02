c058c792510aa3caeda1565744947150d19cdffb
Rob Shoemaker <rob.shoe187@gmail.com>
2021-08-01 20:34:00 -0400

    added template literal

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require('inquirer');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 const Manager = require('./lib/Manager.js');
+const fs = require('fs');
+let template = [];
 
 promptsManager = [
     {
@@ -83,32 +85,97 @@ promptsMenu = [
 const managerPrompt = () => {
     inquirer.prompt(promptsManager).then(function(answer) {
         manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
-        menuPrompt();
+        
+    const managerTemplate = `<div class="card">
+        <div class="container">
+          <h4><b>${manager.name}</b></h4>
+          <p>Manager</p>
+          <ul>
+            <li>ID# ${manager.id}</li>
+            <li><a>${manager.email}</a></li>
+            <li>Office Number: ${manager.officeNumber}</li>
+          </ul>
+        </div>
+      </div>`
+    template.push(managerTemplate);
+    menuPrompt();
     });
 }
 
 const engineerPrompt= () => {
     inquirer.prompt(promptsEngineer).then(function(answer) {
         engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
-        menuPrompt();
+        const engineerTemplate = `<div class="card">
+        <div class="container">
+          <h4><b>${engineer.name}</b></h4>
+          <p>Engineer</p>
+          <ul>
+            <li>ID# ${engineer.id}</li>
+            <li><a>${engineer.email}</a></li>
+            <li>Github:<a>https://github.com/${engineer.github}</a></li>
+          </ul>
+        </div>
+      </div>`
+    template.push(engineerTemplate);
+    menuPrompt();
     });
 }
 const internPrompt= () => {    
     inquirer.prompt(promptsIntern).then(function(answer){
             intern = new Intern(answer.name, answer.id, answer.email, answer.school);
-            menuPrompt();
+        const internTemplate = `<div class="card">
+        <div class="container">
+          <h4><b>${intern.name}</b></h4>
+          <p>Intern</p>
+          <ul>
+            <li>ID# ${intern.id}</li>
+            <li><a>${intern.email}</a></li>
+            <li>School: ${intern.school}</li>
+          </ul>
+        </div>
+      </div>`
+        template.push(internTemplate);
+        menuPrompt();
+    
         });
-    };
-
+};
 const menuPrompt = () => {
     inquirer.prompt(promptsMenu).then(function(answer){
         if(answer.choice === 'Engineer'){
             engineerPrompt();
         } else if(answer.choice === 'Intern') {
             internPrompt();
-        } else{console.log('None')}
+        } else{
+            addToHtml(template);
+        }
     });
+};
+
+const writeToFile = (data) => {
+    fs.writeFile('./src/index.html', data, function (err) {
+    if (err) return console.log(err);
+    console.log('index.html created');
+  });
+};
+
+const addToHtml= (template) => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./styles.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+    <h1>Team Profile</h1>
+    ${template}    
+    </body>
+    </html>`
+    writeToFile(html);
 }
 
 managerPrompt();
+
 
